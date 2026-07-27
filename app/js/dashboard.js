b90536d307538db5f1bb025c2d73eb329e7a135a
tomR96 <tommyryan12@yahoo.ie>
2026-07-28 00:33:30 +0100

    add dashboard data

---
+++ app/js/dashboard.js
@@ -0,0 +1,6 @@
+document.getElementById("matchesPlayed").textContent = 12;
+document.getElementById("wins").textContent = 9;
+document.getElementById("scoresFor").textContent = "24-176";
+document.getElementById("scoresAgainst").textContent = "15-112";
+document.getElementById("pointsDifference").textContent = "+64";
+document.getElementById("winRate").textContent = "75%";
